aa3107ff81a2f17ee89006715b58b10bad1d8f83
CDiala <dialachibuzo@yahoo.com>
2026-05-25 19:12:50 +0100

    feat: Add baseHref to preprod

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -3,10 +3,7 @@ export const environment = {
   isDev: false,
   beBaseUrl: 'http://172.31.60.81/brokersauth/api/',
   docDirectory: './docs',
-<<<<<<< HEAD
   encryptKey: '12345678901234567890123456789012',
   encryptIV: '1234567890123456',
   baseHref: 'brk3rz-prtl-preprod',
-=======
->>>>>>> da035f79435a1dd2d4f313df87f728ba2b8a5733
 };
